73cc9dec18511f0ca01e7942324d5947b1034c35
Jayanth <jayanthpetchetti@gmail.com>
2025-09-19 12:41:25 +0530

    created greatest_of_two.js file
    This file has the javascript code which finds and prints the greatest of two numbers

---
+++ greatestOfTwo/greatest_of_two.js
@@ -0,0 +1,6 @@
+const numberOne = -4;
+const numberTwo = -3;
+
+const greatestNumber = numberOne > numberTwo ? numberOne : numberTwo;
+
+console.log(greatestNumber,"is greatest between", numberOne, "and", numberTwo);
