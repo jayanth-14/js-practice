f799af584ddfe3176a978a452fb85d71a0695a4c
Jayanth <jayanthpetchetti@gmail.com>
2025-09-19 17:47:45 +0530

    Create difference.js file to find difference between two numbers

---
+++ difference/difference.js
@@ -0,0 +1,8 @@
+/*
+  Finding the difference between two numbers
+*/
+const numberOne = -10, numberTwo = 0;
+const greaterNumber = (numberOne > numberTwo) ? numberOne : numberTwo;
+const lessNumber = (numberOne < numberTwo) ? numberOne : numberTwo;
+
+console.log("The difference is :", greaterNumber - lessNumber);
