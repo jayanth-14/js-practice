2a6b661d4cfab776cdc404103d5ef3d068b0a704
Jayanth <jayanthpetchetti@gmail.com>
2025-09-20 14:10:56 +0530

    implemented distance calculating using nested ifs

--- assignments/lion_hunt/lion_hunt.js
+++ assignments/lion_hunt/lion_hunt.js
@@ -1,6 +1,94 @@
-const savannahMap = "LZ";
-let shortestDistance = 0;
-let currentPosition = 0;
+const savannahMap = "L     Z";
 const lengthOfMap = savannahMap.length;
+let shortestDistance = 0;
+let currentPosition = 1;
+let distanceFromLastAnimal = 0;
+let lastAnimal = savannahMap[0];
+
+if (currentPosition < lengthOfMap) {
+  const animalAtCurrentPosition = savannahMap[currentPosition];
+  const notASpace = animalAtCurrentPosition !== " ";
+  if (notASpace && animalAtCurrentPosition !== lastAnimal) {
+    lastAnimal = animalAtCurrentPosition;
+    if ( distanceFromLastAnimal <= distanceFromLastAnimal) {
+      shortestDistance = distanceFromLastAnimal;
+      distanceFromLastAnimal = 0;
+    }
+  }
+  distanceFromLastAnimal = distanceFromLastAnimal + 1;
+  currentPosition = currentPosition + 1;
+if (currentPosition < lengthOfMap) {
+  const animalAtCurrentPosition = savannahMap[currentPosition];
+  const notASpace = animalAtCurrentPosition !== " ";
+  if (notASpace && animalAtCurrentPosition !== lastAnimal) {
+    lastAnimal = animalAtCurrentPosition;
+    if ( distanceFromLastAnimal <= distanceFromLastAnimal) {
+      shortestDistance = distanceFromLastAnimal;
+      distanceFromLastAnimal = 0;
+    }
+  }
+  distanceFromLastAnimal = distanceFromLastAnimal + 1;
+  currentPosition = currentPosition + 1;
+if (currentPosition < lengthOfMap) {
+  const animalAtCurrentPosition = savannahMap[currentPosition];
+  const notASpace = animalAtCurrentPosition !== " ";
+  if (notASpace && animalAtCurrentPosition !== lastAnimal) {
+    lastAnimal = animalAtCurrentPosition;
+    if ( distanceFromLastAnimal <= distanceFromLastAnimal) {
+      shortestDistance = distanceFromLastAnimal;
+      distanceFromLastAnimal = 0;
+    }
+  }
+  distanceFromLastAnimal = distanceFromLastAnimal + 1;
+  currentPosition = currentPosition + 1;
+if (currentPosition < lengthOfMap) {
+  const animalAtCurrentPosition = savannahMap[currentPosition];
+  const notASpace = animalAtCurrentPosition !== " ";
+  if (notASpace && animalAtCurrentPosition !== lastAnimal) {
+    lastAnimal = animalAtCurrentPosition;
+    if ( distanceFromLastAnimal <= distanceFromLastAnimal) {
+      shortestDistance = distanceFromLastAnimal;
+      distanceFromLastAnimal = 0;
+    }
+  }
+  distanceFromLastAnimal = distanceFromLastAnimal + 1;
+  currentPosition = currentPosition + 1;
+if (currentPosition < lengthOfMap) {
+  const animalAtCurrentPosition = savannahMap[currentPosition];
+  const notASpace = animalAtCurrentPosition !== " ";
+  if (notASpace && animalAtCurrentPosition !== lastAnimal) {
+    lastAnimal = animalAtCurrentPosition;
+    if ( distanceFromLastAnimal <= distanceFromLastAnimal) {
+      shortestDistance = distanceFromLastAnimal;
+      distanceFromLastAnimal = 0;
+    }
+  }
+  distanceFromLastAnimal = distanceFromLastAnimal + 1;
+  currentPosition = currentPosition + 1;
+if (currentPosition < lengthOfMap) {
+  const animalAtCurrentPosition = savannahMap[currentPosition];
+  const notASpace = animalAtCurrentPosition !== " ";
+  if (notASpace && animalAtCurrentPosition !== lastAnimal) {
+    lastAnimal = animalAtCurrentPosition;
+    if ( distanceFromLastAnimal <= distanceFromLastAnimal) {
+      shortestDistance = distanceFromLastAnimal;
+      distanceFromLastAnimal = 0;
+    }
+  }
+  distanceFromLastAnimal = distanceFromLastAnimal + 1;
+  currentPosition = currentPosition + 1;
+if (currentPosition < lengthOfMap) {
+  const animalAtCurrentPosition = savannahMap[currentPosition];
+  const notASpace = animalAtCurrentPosition !== " ";
+  if (notASpace && animalAtCurrentPosition !== lastAnimal) {
+    lastAnimal = animalAtCurrentPosition;
+    if ( distanceFromLastAnimal <= distanceFromLastAnimal) {
+      shortestDistance = distanceFromLastAnimal;
+      distanceFromLastAnimal = 0;
+    }
+  }
+  distanceFromLastAnimal = distanceFromLastAnimal + 1;
+  currentPosition = currentPosition + 1;
+}}}}}}}
 
 console.log("The shortest distance between lions and zebras in given savannah map \"",savannahMap,"\" is", shortestDistance);
