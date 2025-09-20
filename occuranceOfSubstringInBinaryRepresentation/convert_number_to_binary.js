6dfdd0c40320f4c34baa1d156b540554af223a8c
Jayanth <jayanthpetchetti@gmail.com>
2025-09-20 09:48:47 +0530

    started writing logic for convertion, instead of manual assignment of binary representation

--- occuranceOfSubstringInBinaryRepresentation/convert_number_to_binary.js
+++ occuranceOfSubstringInBinaryRepresentation/convert_number_to_binary.js
@@ -1,3 +1,8 @@
 const number  = 65;
-const binaryRepresentation = 1000001;
+let copyOfNumber = number;
+let binaryRepresentation = 0;
+if (copyOfNumber > 0) {
+  binaryRepresentation = binaryRepresentation * 10 + copyOfNumber % 2;
+  copyOfNumber = number / 2;
+}
 console.log("The binary representation of",number,"is", binaryRepresentation);
