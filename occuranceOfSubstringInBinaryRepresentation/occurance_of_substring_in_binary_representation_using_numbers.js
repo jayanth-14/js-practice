055335134b6b4a4beed7247be07ad930458d1487
Jayanth <jayanthpetchetti@gmail.com>
2025-09-20 09:34:31 +0530

    used variable "number" to store the input number

--- occuranceOfSubstringInBinaryRepresentation/occurance_of_substring_in_binary_representation_using_numbers.js
+++ occuranceOfSubstringInBinaryRepresentation/occurance_of_substring_in_binary_representation_using_numbers.js
@@ -1,2 +1,3 @@
 const substring = 10;
-console.log("The number of occurance of", substring,"in binary representation of 65 (1000001) is 1");
+const number = 65;
+console.log("The number of occurance of", substring,"in binary representation of",number,"(1000001) is 1");
