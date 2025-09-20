862827f54d5c6ea218cc9b8120c9bceeee19a349
Jayanth <jayanthpetchetti@gmail.com>
2025-09-20 20:20:24 +0530

    implemented counting of binary digits in reversing the binary logic to reduce the number of loops

--- occuranceOfSubstringInBinaryRepresentation/occurance_of_substring_in_binary_representation_using_numbers.js
+++ occuranceOfSubstringInBinaryRepresentation/occurance_of_substring_in_binary_representation_using_numbers.js
@@ -10,11 +10,13 @@ while (copyOfNumber >= 1) {
   copyOfNumber = (copyOfNumber - remainderWhenHalfed) / 2;
 }
 let copyOfBinary = binaryRepresentation;
+let countOfDigitsInBinary = 0;
 binaryRepresentation = 0;
 while (copyOfBinary >= 1) {
   const lastDigit = copyOfBinary % 10;
   binaryRepresentation = binaryRepresentation * 10 + lastDigit;
   copyOfBinary = (copyOfBinary - lastDigit) / 10;
+  countOfDigitsInBinary = countOfDigitsInBinary + 1;
 }
 
 let copyOfSubstring = substring;
@@ -22,13 +24,6 @@ let countOfDigitsInSubstring = 0;
 while (copyOfSubstring >= 1) {
   copyOfSubstring = copyOfSubstring / 10;
   countOfDigitsInSubstring = countOfDigitsInSubstring + 1;
-}
-
-copyOfBinary = binaryRepresentation;
-let countOfDigitsInBinary = 0;
-while (copyOfBinary >= 1) {
-  copyOfBinary = copyOfBinary / 10;
-  countOfDigitsInBinary = countOfDigitsInBinary + 1;
 }
 
 console.log("The number of occurance of", substring,"in binary representation of",number,"(",binaryRepresentation,") is", occuranceCount);
