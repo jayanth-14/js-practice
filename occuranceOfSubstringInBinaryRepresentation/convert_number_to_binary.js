d1d6fb667492348bd555f80db2028037aa763338
Jayanth <jayanthpetchetti@gmail.com>
2025-09-20 10:17:05 +0530

    replaced nested if's with the while loop

--- occuranceOfSubstringInBinaryRepresentation/convert_number_to_binary.js
+++ occuranceOfSubstringInBinaryRepresentation/convert_number_to_binary.js
@@ -1,95 +1,9 @@
 const number = 65;
 let copyOfNumber = number;
 let binaryRepresentation = 0;
-if (copyOfNumber >= 1) {
+while (copyOfNumber >= 1) {
   const remainderWhenHalfed = copyOfNumber % 2;
   binaryRepresentation = binaryRepresentation * 10 + remainderWhenHalfed;
   copyOfNumber = (copyOfNumber - remainderWhenHalfed) / 2;
-  console.log("remainder :", remainderWhenHalfed);
-  console.log("new Number :", copyOfNumber);
-  if (copyOfNumber >= 1) {
-    const remainderWhenHalfed = copyOfNumber % 2;
-    binaryRepresentation = binaryRepresentation * 10 + remainderWhenHalfed;
-    copyOfNumber = (copyOfNumber - remainderWhenHalfed) / 2;
-    console.log("remainder :", remainderWhenHalfed);
-    console.log("new Number :", copyOfNumber);
-    if (copyOfNumber >= 1) {
-      const remainderWhenHalfed = copyOfNumber % 2;
-      binaryRepresentation = binaryRepresentation * 10 + remainderWhenHalfed;
-      copyOfNumber = (copyOfNumber - remainderWhenHalfed) / 2;
-      console.log("remainder :", remainderWhenHalfed);
-      console.log("new Number :", copyOfNumber);
-      if (copyOfNumber >= 1) {
-        const remainderWhenHalfed = copyOfNumber % 2;
-        binaryRepresentation = binaryRepresentation * 10 + remainderWhenHalfed;
-        copyOfNumber = (copyOfNumber - remainderWhenHalfed) / 2;
-        console.log("remainder :", remainderWhenHalfed);
-        console.log("new Number :", copyOfNumber);
-        if (copyOfNumber >= 1) {
-          const remainderWhenHalfed = copyOfNumber % 2;
-          binaryRepresentation = binaryRepresentation * 10 + remainderWhenHalfed;
-          copyOfNumber = (copyOfNumber - remainderWhenHalfed) / 2;
-          console.log("remainder :", remainderWhenHalfed);
-          console.log("new Number :", copyOfNumber);
-          if (copyOfNumber >= 1) {
-            const remainderWhenHalfed = copyOfNumber % 2;
-            binaryRepresentation = binaryRepresentation * 10 + remainderWhenHalfed;
-            copyOfNumber = (copyOfNumber - remainderWhenHalfed) / 2;
-            console.log("remainder :", remainderWhenHalfed);
-            console.log("new Number :", copyOfNumber);
-            if (copyOfNumber >= 1) {
-              const remainderWhenHalfed = copyOfNumber % 2;
-              binaryRepresentation = binaryRepresentation * 10 + remainderWhenHalfed;
-              copyOfNumber = (copyOfNumber - remainderWhenHalfed) / 2;
-              console.log("remainder :", remainderWhenHalfed);
-              console.log("new Number :", copyOfNumber);
-              if (copyOfNumber >= 1) {
-                const remainderWhenHalfed = copyOfNumber % 2;
-                binaryRepresentation = binaryRepresentation * 10 + remainderWhenHalfed;
-                copyOfNumber = (copyOfNumber - remainderWhenHalfed) / 2;
-                console.log("remainder :", remainderWhenHalfed);
-                console.log("new Number :", copyOfNumber);
-                if (copyOfNumber >= 1) {
-                  const remainderWhenHalfed = copyOfNumber % 2;
-                  binaryRepresentation = binaryRepresentation * 10 + remainderWhenHalfed;
-                  copyOfNumber = (copyOfNumber - remainderWhenHalfed) / 2;
-                  console.log("remainder :", remainderWhenHalfed);
-                  console.log("new Number :", copyOfNumber);
-                  if (copyOfNumber >= 1) {
-                    const remainderWhenHalfed = copyOfNumber % 2;
-                    binaryRepresentation = binaryRepresentation * 10 + remainderWhenHalfed;
-                    copyOfNumber = (copyOfNumber - remainderWhenHalfed) / 2;
-                    console.log("remainder :", remainderWhenHalfed);
-                    console.log("new Number :", copyOfNumber);
-                    if (copyOfNumber >= 1) {
-                      const remainderWhenHalfed = copyOfNumber % 2;
-                      binaryRepresentation = binaryRepresentation * 10 + remainderWhenHalfed;
-                      copyOfNumber = (copyOfNumber - remainderWhenHalfed) / 2;
-                      console.log("remainder :", remainderWhenHalfed);
-                      console.log("new Number :", copyOfNumber);
-                      if (copyOfNumber >= 1) {
-                        const remainderWhenHalfed = copyOfNumber % 2;
-                        binaryRepresentation = binaryRepresentation * 10 + remainderWhenHalfed;
-                        copyOfNumber = (copyOfNumber - remainderWhenHalfed) / 2;
-                        console.log("remainder :", remainderWhenHalfed);
-                        console.log("new Number :", copyOfNumber);
-                        if (copyOfNumber >= 1) {
-                          const remainderWhenHalfed = copyOfNumber % 2;
-                          binaryRepresentation = binaryRepresentation * 10 + remainderWhenHalfed;
-                          copyOfNumber = (copyOfNumber - remainderWhenHalfed) / 2;
-                          console.log("remainder :", remainderWhenHalfed);
-                          console.log("new Number :", copyOfNumber);
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
 }
 console.log("The binary representation of", number, "is", binaryRepresentation);
