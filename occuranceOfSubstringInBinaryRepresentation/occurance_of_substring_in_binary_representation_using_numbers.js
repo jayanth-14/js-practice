91d1677b8e5c30d9c404f17ad4db0a921209a798
Jayanth <jayanthpetchetti@gmail.com>
2025-09-20 10:34:34 +0530

    implementing conversion of number to binary

--- occuranceOfSubstringInBinaryRepresentation/occurance_of_substring_in_binary_representation_using_numbers.js
+++ occuranceOfSubstringInBinaryRepresentation/occurance_of_substring_in_binary_representation_using_numbers.js
@@ -1,5 +1,18 @@
 const substring = 10;
-const number = 65;
-const binaryRepresentation = 1000001;
-const occuranceCount = 1;
+const number = 19;
+const occuranceCount = 2;
+let copyOfNumber = number;
+let binaryRepresentation = 0;
+while (copyOfNumber >= 1) {
+  const remainderWhenHalfed = copyOfNumber % 2;
+  binaryRepresentation = binaryRepresentation * 10 + remainderWhenHalfed;
+  copyOfNumber = (copyOfNumber - remainderWhenHalfed) / 2;
+}
+let copyOfBinary = binaryRepresentation;
+binaryRepresentation = 0;
+while (copyOfBinary >= 1) {
+  const lastDigit = copyOfBinary % 10;
+  binaryRepresentation = binaryRepresentation * 10 + lastDigit;
+  copyOfBinary = (copyOfBinary - lastDigit) / 10;
+}
 console.log("The number of occurance of", substring,"in binary representation of",number,"(",binaryRepresentation,") is", occuranceCount);
