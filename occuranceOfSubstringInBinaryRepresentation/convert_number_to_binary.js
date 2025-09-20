e26de97fbd4828c092b8020f49433bb1806a6b51
Jayanth <jayanthpetchetti@gmail.com>
2025-09-20 09:50:16 +0530

    used multiple ifs substituting loop

--- occuranceOfSubstringInBinaryRepresentation/convert_number_to_binary.js
+++ occuranceOfSubstringInBinaryRepresentation/convert_number_to_binary.js
@@ -1,8 +1,39 @@
-const number  = 65;
+const number = 65;
 let copyOfNumber = number;
 let binaryRepresentation = 0;
 if (copyOfNumber > 0) {
   binaryRepresentation = binaryRepresentation * 10 + copyOfNumber % 2;
   copyOfNumber = number / 2;
+  if (copyOfNumber > 0) {
+    binaryRepresentation = binaryRepresentation * 10 + copyOfNumber % 2;
+    copyOfNumber = number / 2;
+    if (copyOfNumber > 0) {
+      binaryRepresentation = binaryRepresentation * 10 + copyOfNumber % 2;
+      copyOfNumber = number / 2;
+      if (copyOfNumber > 0) {
+        binaryRepresentation = binaryRepresentation * 10 + copyOfNumber % 2;
+        copyOfNumber = number / 2;
+        if (copyOfNumber > 0) {
+          binaryRepresentation = binaryRepresentation * 10 + copyOfNumber % 2;
+          copyOfNumber = number / 2;
+
+          if (copyOfNumber > 0) {
+            binaryRepresentation = binaryRepresentation * 10 + copyOfNumber % 2;
+            copyOfNumber = number / 2;
+
+            if (copyOfNumber > 0) {
+              binaryRepresentation = binaryRepresentation * 10 + copyOfNumber % 2;
+              copyOfNumber = number / 2;
+
+              if (copyOfNumber > 0) {
+                binaryRepresentation = binaryRepresentation * 10 + copyOfNumber % 2;
+                copyOfNumber = number / 2;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 }
-console.log("The binary representation of",number,"is", binaryRepresentation);
+console.log("The binary representation of", number, "is", binaryRepresentation);
