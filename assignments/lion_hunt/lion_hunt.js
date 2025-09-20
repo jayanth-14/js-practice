543348e279497384fd0d3bff47415e72381cd8ac
Jayanth <jayanthpetchetti@gmail.com>
2025-09-20 13:48:33 +0530

    stored map length in a variable

--- assignments/lion_hunt/lion_hunt.js
+++ assignments/lion_hunt/lion_hunt.js
@@ -1,6 +1,6 @@
 const savannahMap = "LZ";
 let shortestDistance = 0;
 let currentPosition = 0;
-
+const lengthOfMap = savannahMap.length;
 
 console.log("The shortest distance between lions and zebras in given savannah map \"",savannahMap,"\" is", shortestDistance);
