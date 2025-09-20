febb78adb9517c4a52f3a37562145eac38cf3aab
Jayanth <jayanthpetchetti@gmail.com>
2025-09-20 09:31:22 +0530

    using variable to store substring value

--- occuranceOfSubstringInBinaryRepresentation/occurance_of_substring_in_binary_representation_using_numbers.js
+++ occuranceOfSubstringInBinaryRepresentation/occurance_of_substring_in_binary_representation_using_numbers.js
@@ -1,1 +1,2 @@
-console.log("The number of occurance of 10 in binary representation of 65 (1000001) is 1");
+const substring = 10;
+console.log("The number of occurance of", substring,"in binary representation of 65 (1000001) is 1");
