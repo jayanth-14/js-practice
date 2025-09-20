727f4a77054015cda6f42a168a2e340f70279ff9
Jayanth <jayanthpetchetti@gmail.com>
2025-09-20 13:47:06 +0530

    changed shortestDistance from const to let and created a new variable to store current position

--- assignments/lion_hunt/lion_hunt.js
+++ assignments/lion_hunt/lion_hunt.js
@@ -1,4 +1,6 @@
 const savannahMap = "LZ";
-const shortestDistance = 0;
+let shortestDistance = 0;
+let currentPosition = 0;
+
 
 console.log("The shortest distance between lions and zebras in given savannah map \"",savannahMap,"\" is", shortestDistance);
