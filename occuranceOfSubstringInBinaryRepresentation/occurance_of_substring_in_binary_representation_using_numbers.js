16ea29401292c5cec64b6f587af71b216679b938
Jayanth <jayanthpetchetti@gmail.com>
2025-09-20 09:38:04 +0530

    used occuranceCount variable to store the count of substring occurance

--- occuranceOfSubstringInBinaryRepresentation/occurance_of_substring_in_binary_representation_using_numbers.js
+++ occuranceOfSubstringInBinaryRepresentation/occurance_of_substring_in_binary_representation_using_numbers.js
@@ -1,4 +1,5 @@
 const substring = 10;
 const number = 65;
 const binaryRepresentation = 1000001;
-console.log("The number of occurance of", substring,"in binary representation of",number,"(",binaryRepresentation,") is 1");
+const occuranceCount = 1;
+console.log("The number of occurance of", substring,"in binary representation of",number,"(",binaryRepresentation,") is", occuranceCount);
