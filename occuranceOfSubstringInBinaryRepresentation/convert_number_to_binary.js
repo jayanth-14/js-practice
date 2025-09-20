ca96efa6f6dd1609d10b8476b66184c5add0584c
Jayanth <jayanthpetchetti@gmail.com>
2025-09-20 09:44:10 +0530

    used variable "binaryRepresentation" to store the binary number

--- occuranceOfSubstringInBinaryRepresentation/convert_number_to_binary.js
+++ occuranceOfSubstringInBinaryRepresentation/convert_number_to_binary.js
@@ -1,2 +1,3 @@
 const number  = 65;
-console.log("The binary representation of",number,"is 1000001");
+const binaryRepresentation = 1000001;
+console.log("The binary representation of",number,"is", binaryRepresentation);
