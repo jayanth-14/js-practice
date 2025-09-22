5e06e5903bada562358d7e5f2fdabf5cbfcc96c2
Jayanth <jayanthpetchetti@gmail.com>
2025-09-22 09:54:29 +0530

    implemented loop logic for 1 iteration

--- practice/numbers/numberOfDivisors/find_number_of_divisors.js
+++ practice/numbers/numberOfDivisors/find_number_of_divisors.js
@@ -1,3 +1,10 @@
 const number = 0;
-const countOfDivisors = 0;
+let countOfDivisors = 0;
+let counter = 1;
+if (counter <= number ){
+  if (number%counter === 0) {
+    countOfDivisors = countOfDivisors + 1;
+  }
+  counter = counter + 1;
+}
 console.log("The number of divisors for the number",number,"is", countOfDivisors);
