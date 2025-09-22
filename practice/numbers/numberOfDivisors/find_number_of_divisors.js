4ef26d81e2035010f8febcc37a5bddfd80c08bb1
Jayanth <jayanthpetchetti@gmail.com>
2025-09-22 09:55:28 +0530

    Implemented loop logic with nested if statements

--- practice/numbers/numberOfDivisors/find_number_of_divisors.js
+++ practice/numbers/numberOfDivisors/find_number_of_divisors.js
@@ -1,4 +1,4 @@
-const number = 0;
+const number = 6;
 let countOfDivisors = 0;
 let counter = 1;
 if (counter <= number ){
@@ -6,5 +6,66 @@ if (counter <= number ){
     countOfDivisors = countOfDivisors + 1;
   }
   counter = counter + 1;
-}
+if (counter <= number ){
+  if (number%counter === 0) {
+    countOfDivisors = countOfDivisors + 1;
+  }
+  counter = counter + 1;
+if (counter <= number ){
+  if (number%counter === 0) {
+    countOfDivisors = countOfDivisors + 1;
+  }
+  counter = counter + 1;
+if (counter <= number ){
+  if (number%counter === 0) {
+    countOfDivisors = countOfDivisors + 1;
+  }
+  counter = counter + 1;
+if (counter <= number ){
+  if (number%counter === 0) {
+    countOfDivisors = countOfDivisors + 1;
+  }
+  counter = counter + 1;
+if (counter <= number ){
+  if (number%counter === 0) {
+    countOfDivisors = countOfDivisors + 1;
+  }
+  counter = counter + 1;
+if (counter <= number ){
+  if (number%counter === 0) {
+    countOfDivisors = countOfDivisors + 1;
+  }
+  counter = counter + 1;
+if (counter <= number ){
+  if (number%counter === 0) {
+    countOfDivisors = countOfDivisors + 1;
+  }
+  counter = counter + 1;
+if (counter <= number ){
+  if (number%counter === 0) {
+    countOfDivisors = countOfDivisors + 1;
+  }
+  counter = counter + 1;
+if (counter <= number ){
+  if (number%counter === 0) {
+    countOfDivisors = countOfDivisors + 1;
+  }
+  counter = counter + 1;
+if (counter <= number ){
+  if (number%counter === 0) {
+    countOfDivisors = countOfDivisors + 1;
+  }
+  counter = counter + 1;
+if (counter <= number ){
+  if (number%counter === 0) {
+    countOfDivisors = countOfDivisors + 1;
+  }
+  counter = counter + 1;
+if (counter <= number ){
+  if (number%counter === 0) {
+    countOfDivisors = countOfDivisors + 1;
+  }
+  counter = counter + 1;
+}}}}}}}}}}}}}
 console.log("The number of divisors for the number",number,"is", countOfDivisors);
+
