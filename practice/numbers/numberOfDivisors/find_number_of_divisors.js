9773bf736ecb7a5ce506d3b2cefad97fa1e42b74
Jayanth <jayanthpetchetti@gmail.com>
2025-09-22 09:58:25 +0530

    Changed while loop with for loop

--- practice/numbers/numberOfDivisors/find_number_of_divisors.js
+++ practice/numbers/numberOfDivisors/find_number_of_divisors.js
@@ -1,10 +1,8 @@
 const number = 7;
 let countOfDivisors = 0;
-let counter = 1;
-while (counter <= number ){
+for (let counter = 1; counter <= number; counter++){
   if (number%counter === 0) {
     countOfDivisors = countOfDivisors + 1;
   }
-  counter = counter + 1;
 }
 console.log("The number of divisors for the number",number,"is", countOfDivisors);
