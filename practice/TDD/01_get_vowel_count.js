0e929f1c21fcc27fdc32b468231a000ced1fda3e
Jayanth <jayanthpetchetti@gmail.com>
2025-09-25 09:22:18 +0530

    started developing function to get vowel count using testing driven development

---
+++ practice/TDD/01_get_vowel_count.js
@@ -0,0 +1,26 @@
+function getVowelCount(string) {
+  return string;
+}
+
+// testing logic starts here
+function testResultSymbol (result, expectedOutput) {
+  return (result === expectedOutput) ? "✅" : "❌" ;
+}
+
+function testFunction(testInput, expectedOutput) {
+  const result = getVowelCount(testInput);
+  const resultSymbol = testResultSymbol(result, expectedOutput);
+  const testIntro = resultSymbol +" The count of vowels in " + testInput;
+  console.log(testIntro, "is", expectedOutput, "and we got", result);
+}
+
+function startTest() {
+  console.log("Testing \"Finding the count of vowels in given string\"");
+  testFunction("testInput", 3);
+  testFunction("aeiou", 5);
+  testFunction("cry", 0);
+  testFunction(1010, 0);
+  // testFunction(testInput, expectedOutput);
+}
+
+startTest();
