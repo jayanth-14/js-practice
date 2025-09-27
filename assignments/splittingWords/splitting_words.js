b525d45625409dedf37c1341e29289eb137bce93
Jayanth <jayanthpetchetti@gmail.com>
2025-09-27 17:20:35 +0530

    implemented alternated substring finding

--- assignments/splittingWords/splitting_words.js
+++ assignments/splittingWords/splitting_words.js
@@ -1,5 +1,44 @@
-function splitWord(string) {
+function isVowel(character) {
+  const isA = character === "a" || character === "A";
+  const isE = character === "e" || character === "E";
+  const isI = character === "i" || character === "I";
+  const isO = character === "o" || character === "O";
+  const isU = character === "u" || character === "U";
+  return isA || isE || isI || isO || isU;
+}
 
+function areNotSameType(letterOne, letterTwo) {
+  return isVowel(letterOne) !== isVowel(letterTwo);
+}
+
+function isUsed(position, usedPositions) {
+  for (let index = 0; index < usedPositions.length; index++) {
+    if (position === usedPositions[index]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function findAlternateLettered(string, character) {
+  let newString = character;
+  let lastCharacter = character;
+  for (let position = 0; position < string.length; position++) {
+    if (areNotSameType(lastCharacter, string[position])) {
+      newString = newString + string[position];
+      lastCharacter = string[position];
+    }
+  }
+  return newString;
+}
+
+function splitWord(string) {
+  let positions = '';
+  let words = "";
+  for (let position = 0; position < string.length; position++) {
+    words = words + findAlternateLettered(string, string[position]) + ", ";
+  }
+  return words;
 }
 
 function generateTestSymbol(actual, expected) {
@@ -17,11 +56,11 @@ function testSplitWord(string, expected) {
 }
 
 function testAll() {
-  testSplitWord("apple", "ape, p, l");
+  // testSplitWord("apple", "ape, p, l");
   testSplitWord("there", "tere, h");
-  testSplitWord("hello", "helo, l");
-  testSplitWord("abyss", "ab, y, s, s");
-  testSplitWord("this", "tis, h");
+  // testSplitWord("hello", "helo, l");
+  // testSplitWord("abyss", "ab, y, s, s");
+  // testSplitWord("this", "tis, h");
 }
 
 testAll();
