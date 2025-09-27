47858c3e05b767dcb20012da18e9d7892f64d4ae
Jayanth <jayanthpetchetti@gmail.com>
2025-09-27 14:19:54 +0530

    corrected second condition

--- assignments/conditions/correcting_conditions.js
+++ assignments/conditions/correcting_conditions.js
@@ -7,3 +7,13 @@
 const temperature = 43;
 const weather = temperature > 30 ? 'hot' : 'cool';
 console.log(weather);
+
+// if (isMember) {
+//   discount = 10;
+// } else {
+//   discount = 0;
+// }
+
+const isMember = true;
+const discount = isMember ? 10 : 0;
+console.log(discount);
