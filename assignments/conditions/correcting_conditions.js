1bdd400cc54eca3575b0d20fa4732646a4c6fbf4
Jayanth <jayanthpetchetti@gmail.com>
2025-09-27 14:17:38 +0530

    corrected the first condition

---
+++ assignments/conditions/correcting_conditions.js
@@ -0,0 +1,9 @@
+// if (temperature > 30) {
+//   weather = "hot";
+// } else {
+//   weather = "cool";
+// }
+
+const temperature = 43;
+const weather = temperature > 30 ? 'hot' : 'cool';
+console.log(weather);
